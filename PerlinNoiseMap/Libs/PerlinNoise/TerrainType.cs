276a8e8de09e4acdb4d8604330bd339aeae138aa
Nono02P <nono02p@hotmail.fr>
2019-11-28 02:37:35 +0100

    Ajout des types de terrains.

---
+++ PerlinNoiseMap/Libs/PerlinNoise/TerrainType.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Libs.PerlinNoise
+{
+    public struct TerrainType
+    {
+        public string Name;
+        public float Height;
+        public Color Color;
+    }
+}
